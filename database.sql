31c2d6dcfa43bae36c711a3be12780bceb2d8480
s1rojov <sirojoff2727@gmail.com>
2024-04-13 03:42:50 +0500

    database structure created

--- database.sql
+++ database.sql
@@ -3,3 +3,39 @@ create table organization(
     fullname varchar(255) not null, 
     shortname varchar(255)
 )
+
+create table part(
+    id bigserial not null primary key, 
+    fullname varchar(255) not null, 
+    shortname varchar(255)
+    org_id bigint references organization(id)
+)
+
+create table section(
+    id bigserial not null primary key, 
+    fullname varchar(255) not null, 
+    shortname varchar(255)
+    part_id bigint references part(id)
+)
+
+create table employee_type(
+    id bigserial not null primary key, 
+    name varchar(255) not null,
+    count integer not null,
+    section_id bigint references section(id)
+)
+
+create table employee(
+    id bigserial not null primary key, 
+    fullname varchar(255) not null,
+    type_id bigint references employee_type(id),
+    section_id bigint references section(id)
+)
+
+create table admin(
+    id bigserial not null primary key, 
+    fullname varchar(255) not null,
+    login varchar(50) not null,
+    password varchar(50) not null,
+    org_id bigint references organization(id)
+)
--- routes/organization.js
+++ routes/organization.js
@@ -3,6 +3,9 @@ const pool = require("../config/db");
 const router = Router();
 
 // all logic here
-
+router.get('/', async (req, res) => {
+    const organizations = await pool.query('select * from organization')
+    res.status(200).json(organizations.rows)
+})
 
 module.exports = router
